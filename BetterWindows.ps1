ba73cca639d5cf2f42020f9530d1ede5da314b1d
HardcodeCoder <hardcodecoder@gmail.com>
2025-05-19 23:17:30 +0530

    Move utility functions to `Utils.ps1`

--- BetterWindows.ps1
+++ BetterWindows.ps1
@@ -24,6 +24,9 @@
 #>
 
 
+# Source Utils
+. .\Utils.ps1
+
 # Config file path
 $RegistryConfigFile = Join-Path -Path $PSScriptRoot -ChildPath "config\tweaks.reg"
 $ServiceConfigFile = Join-Path -Path $PSScriptRoot -ChildPath "config\services.json"
@@ -61,99 +64,6 @@ function Show-MainMenu {
     Write-CenteredText "[5] Perform System cleanup                       [e] Install Winget                       "
     Write-CenteredText "[6] Disable Windows Defender                                                              "
     Write-Host ""
-}
-
-# Initialize powershell window for administrator look
-function Initialize-PowershellWindow {
-    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + " - Administrator"
-    $Host.UI.RawUI.BackgroundColor = "Black"
-    $Host.PrivateData.ProgressBackgroundColor = "Black"
-    $Host.PrivateData.ProgressForegroundColor = "White"
-    Clear-Host
-}
-
-# Write text to standard output but centered
-function Write-CenteredText {
-    param (
-        [string]$Text,
-        [string]$ForegroundColor = 'White'
-    )
-
-    $consoleWidth = [Console]::WindowWidth
-    $padding = [math]::Floor(($consoleWidth - $Text.Length) / 2)
-    $paddedText = ' ' * $padding + $Text
-
-    Write-Host $paddedText -ForegroundColor $ForegroundColor
-}
-
-# Write current task header
-function Write-TaskHeader {
-    param (
-        [string]$TaskName
-    )
-
-    $decorator = '-' * 62
-    $text = ' ' * ((62 - $TaskName.Length) / 2) + $TaskName
-    Write-Host $decorator
-    Write-Host $text
-    Write-Host $decorator
-    Write-Host ""
-}
-
-# Write task footer divider
-function Write-TaskFooter {
-    $decorator = '-' * 62
-    Write-Host $decorator
-    Write-Host ""
-}
-
-# Write exception message as warning
-function Write-UnhandledException {
-    param (
-        [string]$Description,
-        [System.Exception]$Exception
-    )
-
-    Write-Warning "$Description - $($Exception.Message)"
-    Write-Warning $Exception.StackTrace
-}
-
-# Deserialize json from file to powershell object
-function ConvertFrom-JsonFie {
-    param (
-        [string]$Path
-    )
-
-    if ($Path -eq '' -Or !(Test-Path -Path $Path)) {
-        Write-Host "File not found: $Path" -ForegroundColor Red
-        return $null
-    }
-
-    $json = Get-Content -Path $Path -Raw
-    return ConvertFrom-Json -InputObject $json
-}
-
-# Utility to download file silently to improve downlad speed
-function Invoke-FileDownload {
-    param (
-        [string]$Uri,
-        [string]$OutFile
-    )
-
-    try {
-        $directory = Split-Path -Path $OutFile -Parent
-
-        if (!(Test-Path -Path $directory)) {
-            New-Item -ItemType Directory -Path $directory -Force | Out-Null
-        }
-
-        Write-Host "Downloading: $Uri"
-        Invoke-RestMethod -Uri $Uri -OutFile $OutFile
-        Write-Host "Download location: $OutFile"
-    }
-    catch {
-        Write-UnhandledException -Description "Unable to downlad file" -E $_.Exception
-    }
 }
 
 # Apply all recommended tweaks
---
+++ Utils.ps1
@@ -0,0 +1,115 @@
+<#
+.SYNOPSIS
+    Utility script for common helper functions
+
+.DESCRIPTION
+    PowerShell script to house common utility functions.
+    This is meant to be sourced in other scripts and does not
+    perform any task in itself
+	
+.EXAMPLE
+    Source this file in other scripts using the "." notation - ". .\Utils.ps1"
+
+.NOTES
+    Author: HardcodeCoder
+    Created: 19 May 2025
+    Last Modified: 19 May 2025
+    Version: 1.0.0
+    Required Modules: PowerShell Remoting
+
+.LINK
+    https://docs.microsoft.com/en-us/powershell/
+#>
+
+# Initialize powershell window for administrator look
+function Initialize-PowershellWindow {
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + " - Administrator"
+    $Host.UI.RawUI.BackgroundColor = "Black"
+    $Host.PrivateData.ProgressBackgroundColor = "Black"
+    $Host.PrivateData.ProgressForegroundColor = "White"
+    Clear-Host
+}
+
+# Write text to standard output but centered
+function Write-CenteredText {
+    param (
+        [string]$Text,
+        [string]$ForegroundColor = 'White'
+    )
+
+    $consoleWidth = [Console]::WindowWidth
+    $padding = [math]::Floor(($consoleWidth - $Text.Length) / 2)
+    $paddedText = ' ' * $padding + $Text
+
+    Write-Host $paddedText -ForegroundColor $ForegroundColor
+}
+
+# Write current task header
+function Write-TaskHeader {
+    param (
+        [string]$TaskName
+    )
+
+    $decorator = '-' * 62
+    $text = ' ' * ((62 - $TaskName.Length) / 2) + $TaskName
+    Write-Host $decorator
+    Write-Host $text
+    Write-Host $decorator
+    Write-Host ""
+}
+
+# Write task footer divider
+function Write-TaskFooter {
+    $decorator = '-' * 62
+    Write-Host $decorator
+    Write-Host ""
+}
+
+# Write exception message as warning
+function Write-UnhandledException {
+    param (
+        [string]$Description,
+        [System.Exception]$Exception
+    )
+
+    Write-Warning "$Description - $($Exception.Message)"
+    Write-Warning $Exception.StackTrace
+}
+
+# Deserialize json from file to powershell object
+function ConvertFrom-JsonFie {
+    param (
+        [string]$Path
+    )
+
+    if ($Path -eq '' -Or !(Test-Path -Path $Path)) {
+        Write-Host "File not found: $Path" -ForegroundColor Red
+        return $null
+    }
+
+    $json = Get-Content -Path $Path -Raw
+    return ConvertFrom-Json -InputObject $json
+}
+
+# Utility to download file silently to improve downlad speed
+function Invoke-FileDownload {
+    param (
+        [string]$Uri,
+        [string]$OutFile
+    )
+
+    try {
+        $directory = Split-Path -Path $OutFile -Parent
+
+        if (!(Test-Path -Path $directory)) {
+            New-Item -ItemType Directory -Path $directory -Force | Out-Null
+        }
+
+        Write-Host "Downloading: $Uri"
+        Invoke-RestMethod -Uri $Uri -OutFile $OutFile
+        Write-Host "Download location: $OutFile"
+    }
+    catch {
+        Write-UnhandledException -Description "Unable to downlad file" -E $_.Exception
+    }
+}
